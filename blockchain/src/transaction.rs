7ccae5d7222593a63b834cf11cf7d4187d1f696f
Maxim Darré <maxim.darre@gmail.com>
2024-04-22 21:08:08 -0400

    feat: implements basic block struct

--- blockchain/src/transaction.rs
+++ blockchain/src/transaction.rs
@@ -1,18 +1,26 @@
+use serde::{Deserialize, Serialize};
+
+/// Represents an account creation transaction
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct AccountCreation {
     pub id: String,
-    pub created_at: u64,
+    pub created_at: i64,
     pub account_id: String,
-    pub initial_balance: u64,
+    pub initial_balance: i64,
 }
 
+// Represents a transfer transaction
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Transfer {
     pub id: String,
-    pub created_at: u64,
+    pub created_at: i64,
     pub from_account: String,
     pub to_account: String,
-    pub amount: u64,
+    pub amount: i64,
 }
 
+/// Represents a transaction that can be added to a block
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum Transaction {
     AccountCreation(AccountCreation),
     Transfer(Transfer),
